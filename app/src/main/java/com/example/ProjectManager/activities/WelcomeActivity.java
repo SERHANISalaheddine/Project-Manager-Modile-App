76961ca431be17f3783c1170a5864359623da025
Jihane Fettoukh <jihanefettoukh8@gmail.com>
2026-01-11 13:00:14 +0100

    Fix AndroidManifest and WelcomeActivity

--- app/src/main/java/com/example/ProjectManager/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/ProjectManager/activities/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package com.example.ProjectManager;
+package com.example.ProjectManager.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ProjectManager.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
